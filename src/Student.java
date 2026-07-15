fa98226667645fb52ceb640ee64ea1bf41d8b31a
25mx332 <25mx332@psgtech.ac.in>
2026-07-15 10:52:54 +0530

    another commit

---
+++ src/Student.java
@@ -0,0 +1,9 @@
+import java.util.*
+class Student {
+    String name;
+    int age;
+    Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
---
+++ src/StudentOperations.java
@@ -0,0 +1,5 @@
+import java.util.*
+class StudentOperations {
+    System.out.println("Enter the number of students:");
+}
+// java code
