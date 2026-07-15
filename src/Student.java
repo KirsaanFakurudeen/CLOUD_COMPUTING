47da66015ac152885e62a71cceaac4e0a16ebded
25mx332 <25mx332@psgtech.ac.in>
2026-07-15 10:58:46 +0530

    created student class

--- src/Student.java
+++ src/Student.java
@@ -1,9 +1,30 @@
-import java.util.*
-class Student {
-    String name;
-    int age;
-    Student(String name, int age) {
+public class Student {
+    private int id;
+    private String name;
+    private String department;
+
+    public Student(int id, String name, String department) {
+        this.id = id;
         this.name = name;
-        this.age = age;
+        this.department = department;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id +
+               ", Name: " + name +
+               ", Department: " + department;
     }
 }
